4f60fb5da08308ba5ee61073978b16567cf12fe1
Edson <79104686+EdsonNeto2533@users.noreply.github.com>
2022-03-12 23:40:40 -0300

    feat(CRIPMARKET-3): add lifeCycle version to gradle

--- app/src/main/java/com/mctable/criptomarket/dashboard/ui/fragment/DashboardFragment.kt
+++ app/src/main/java/com/mctable/criptomarket/dashboard/ui/fragment/DashboardFragment.kt
@@ -1,6 +1,9 @@
 package com.mctable.criptomarket.dashboard.ui.fragment
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.mctable.criptomarket.R
 import com.mctable.criptomarket.dashboard.ui.viewmodel.DashboardViewModel
 import com.mctable.criptomarket.databinding.FragmentDashboardBinding
@@ -10,5 +13,14 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
 
     private val viewModel: DashboardViewModel by viewModel()
     private lateinit var binding: FragmentDashboardBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadObservers()
+    }
+
+    private fun loadObservers(){
+        lifecycleScope.launchWhenStarted {  }
+    }
 
 }
