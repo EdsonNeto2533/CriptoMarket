5b5b7d775a21ee4efcca264f87624575e4e19037
Edson <79104686+EdsonNeto2533@users.noreply.github.com>
2022-03-20 15:04:29 -0300

    feat(CRIPMARKET-7): loading fragment implementation created

--- app/src/main/java/com/mctable/criptomarket/commons/ui/dialogs/LoadingDialog.kt
+++ app/src/main/java/com/mctable/criptomarket/commons/ui/dialogs/LoadingDialog.kt
@@ -1,14 +1,37 @@
 package com.mctable.criptomarket.commons.ui.dialogs
 
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
 import com.mctable.criptomarket.R
+import com.mctable.criptomarket.databinding.FragmentLoadingDialogBinding
 
-class LoadingDialog : DialogFragment(R.layout.fragment_dashboard) {
+class LoadingDialog(private val activity: AppCompatActivity) :
+    DialogFragment() {
 
+    companion object {
+        private const val TAG = "loading_dialog"
+    }
+
+    private lateinit var binding: FragmentLoadingDialogBinding
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val view = activity.layoutInflater.inflate(R.layout.fragment_loading_dialog, null)
+        binding = FragmentLoadingDialogBinding.bind(view)
+
+        val dialog = AlertDialog.Builder(activity)
+        dialog.setView(view)
+
+        return dialog.create()
+    }
+
+    fun show() {
+        this.show(activity.supportFragmentManager, TAG)
+    }
 
 }
--- app/src/main/java/com/mctable/criptomarket/dashboard/ui/fragment/DashboardFragment.kt
+++ app/src/main/java/com/mctable/criptomarket/dashboard/ui/fragment/DashboardFragment.kt
@@ -1,25 +1,24 @@
 package com.mctable.criptomarket.dashboard.ui.fragment
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.mctable.criptomarket.R
-import com.mctable.criptomarket.commons.utils.implementations.UIState
+import com.mctable.criptomarket.commons.ui.dialogs.LoadingDialog
 import com.mctable.criptomarket.dashboard.ui.viewmodel.DashboardViewModel
 import com.mctable.criptomarket.databinding.FragmentDashboardBinding
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.onStart
+import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import org.koin.core.parameter.parametersOf
 
 class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
 
     private val viewModel: DashboardViewModel by viewModel()
     private lateinit var binding: FragmentDashboardBinding
+    private val loadingDialog: LoadingDialog by inject { parametersOf(requireActivity()) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -30,10 +29,11 @@ class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
     private fun loadObservers() {
         lifecycleScope.launchWhenStarted {
             viewModel.coinsListUIState.onStart {
-                print("carregando")
+                loadingDialog.show()
             }.catch {
                 print("falhou")
             }.collect {
+                loadingDialog.dismiss()
                 print(it)
             }
         }
