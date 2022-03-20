b05b55d85704068e974f6390c6f38cfa4f50c219
Edson <79104686+EdsonNeto2533@users.noreply.github.com>
2022-03-20 14:40:26 -0300

    feat(CRIPMARKET-7): loading fragment layout created

---
+++ app/src/main/java/com/mctable/criptomarket/commons/ui/dialogs/LoadingDialog.kt
@@ -0,0 +1,14 @@
+package com.mctable.criptomarket.commons.ui.dialogs
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.mctable.criptomarket.R
+
+class LoadingDialog : DialogFragment(R.layout.fragment_dashboard) {
+
+
+}
