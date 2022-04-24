5495779b104a3223f662832d98348c7062654134
Edson <79104686+EdsonNeto2533@users.noreply.github.com>
2022-04-24 01:30:23 -0300

    feat(CRIPMARKET-8): default error dialog implemented on dashboard

--- app/src/main/java/com/mctable/criptomarket/commons/ui/dialogs/ErrorDialog.kt
+++ app/src/main/java/com/mctable/criptomarket/commons/ui/dialogs/ErrorDialog.kt
@@ -3,10 +3,15 @@ package com.mctable.criptomarket.commons.ui.dialogs
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.mctable.criptomarket.R
 import com.mctable.criptomarket.databinding.FragmentErrorDialogBinding
+import org.koin.core.component.getScopeId
 
 data class ErrorDialogInfoModel(
     val text: String?,
@@ -16,7 +21,7 @@ data class ErrorDialogInfoModel(
 class ErrorDialog(
     private val activity: AppCompatActivity,
     private val errorDialogInfoModel: ErrorDialogInfoModel
-) : DialogFragment() {
+) : BottomSheetDialogFragment() {
 
     companion object {
         private const val ERROR_DIALOG_TAG = "error_dialog"
@@ -24,16 +29,18 @@ class ErrorDialog(
 
     private lateinit var binding: FragmentErrorDialogBinding
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val view = activity.layoutInflater.inflate(R.layout.fragment_loading_dialog, null)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         binding = FragmentErrorDialogBinding.bind(view)
-
-        val dialog = AlertDialog.Builder(activity)
-        dialog.setView(view)
-
         setupDialogInfo()
+    }
 
-        return super.onCreateDialog(savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return activity.layoutInflater.inflate(R.layout.fragment_error_dialog, null)
     }
 
     private fun setupDialogInfo() {
@@ -42,7 +49,7 @@ class ErrorDialog(
         }
 
         binding.mbtTryAgain.setOnClickListener {
-            errorDialogInfoModel.buttonAction
+            errorDialogInfoModel.buttonAction.invoke()
             dismiss()
         }
     }
